bdb4c6013f93795ced589929ab68cddf50457ea8
Marcin <wanago.marcin@gmail.com>
2023-03-02 00:15:50 +0100

    test(getPostsSortedByTitle): test the most basic case

--- src/utilities/getPostsSortedByTitle.js
+++ src/utilities/getPostsSortedByTitle.js
@@ -4,11 +4,9 @@ export async function getPostsSortedByTitle() {
   try {
     const posts = await getPosts();
 
-    const sortedPosts = posts.sort((firstPost, secondPost) => {
+    return posts.sort((firstPost, secondPost) => {
       return secondPost.title.length - firstPost.title.length;
     });
-
-    return sortedPosts[0];
   } catch {
     return [];
   }
