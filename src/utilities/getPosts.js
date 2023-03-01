89d0ddae1e60fd6fd6abf889d256aaa564fce35d
Marcin <wanago.marcin@gmail.com>
2023-03-01 23:54:32 +0100

    feat(getPostWithLongestTitle): add the getPostWithLongestTitle function

---
+++ src/utilities/getPosts.js
@@ -0,0 +1,7 @@
+export async function getPosts() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!response.ok) {
+    throw new Error('Something went wrong when fetching posts');
+  }
+  return response.json();
+}
